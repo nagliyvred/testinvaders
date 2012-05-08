d8f9a31dfcb361546b3fbfabe44ca465f42b064a
Scott Robinson <sr@thoughtworks.com>
2012-05-08 15:11:42 +0800

    Drop requirejs experiment.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,75 +1,75 @@
-require([
-    "require",
-    "ace/ace",
-    "runner",
+function runner(print, name) {
+  return {
+    run: function(scripts, callback){
+      $("#" + name).remove();
 
-    "order!/public/javascript/ext/zepto.min.js",
-    "order!/public/javascript/ext/bootstrap-dropdown.js"], function(require, ace, runner) {
+      var sandbox = $('<iframe />').attr({id: name, src: "runner.html"}).bind('load', function(){ 
+        this.contentWindow.run(scripts, print, callback);
+      }).appendTo(document.body);
+    }
+  }
+}
 
-  $(function() {
-    var src, spec, out;
+$(function() {
+  var src, spec, out;
 
-    var iframe = document.getElementById('game');
+  var iframe = document.getElementById('game');
 
-    $(iframe).bind('load', function() {
-      this.contentWindow.eval(src.getSession().getValue());
-    });
+  $(iframe).bind('load', function() {
+    this.contentWindow.eval(src.getSession().getValue());
+  });
 
-    $(".chrome").hide();
+  $(".chrome").hide();
 
-    $(".btn").click(function(e) {
-      $(".ui").toggle();
-      $(".chrome").toggle();
+  $(".btn").click(function(e) {
+    $(".ui").toggle();
+    $(".chrome").toggle();
 
-      e.preventDefault();
-    });
+    e.preventDefault();
+  });
 
-    require(["ace/mode-javascript", "ace/theme-twilight"], function() {
-      var JavaScriptMode = require("ace/mode/javascript").Mode;
-      var themeTwilight = require("ace/theme-twilight");
+  var JavaScriptMode = require("ace/mode/javascript").Mode;
 
-      src = ace.edit("src");
-      src.setTheme("ace/theme/twilight");
-      src.getSession().setMode(new JavaScriptMode());
-      src.setShowPrintMargin(false);
+  src = ace.edit("src");
+  src.setTheme("ace/theme/twilight");
+  src.getSession().setMode(new JavaScriptMode());
+  src.setShowPrintMargin(false);
 
-      spec = ace.edit("spec");
-      spec.setTheme("ace/theme/twilight");
-      spec.getSession().setMode(new JavaScriptMode());
-      spec.setShowPrintMargin(false);
+  spec = ace.edit("spec");
+  spec.setTheme("ace/theme/twilight");
+  spec.getSession().setMode(new JavaScriptMode());
+  spec.setShowPrintMargin(false);
 
-      out = ace.edit("out");
-      out.setTheme("ace/theme/twilight");
-      out.setReadOnly(true);
-      out.setHighlightActiveLine(false);
-      out.setShowPrintMargin(false);
-      out.setPrintMarginColumn(false);
-      out.renderer.setShowGutter(false);
-      out.renderer.hideCursor(true);
+  out = ace.edit("out");
+  out.setTheme("ace/theme/twilight");
+  out.setReadOnly(true);
+  out.setHighlightActiveLine(false);
+  out.setShowPrintMargin(false);
+  out.setPrintMarginColumn(false);
+  out.renderer.setShowGutter(false);
+  out.renderer.hideCursor(true);
 
-      var scheduledRuns = [];
+  var scheduledRuns = [];
 
-      setInterval(function(){
-        var r = scheduledRuns.pop();
-        r && r();
-      }, 1000);
+  setInterval(function(){
+    var r = scheduledRuns.pop();
+    r && r();
+  }, 1000);
 
-      function delayedRun(){
-        scheduledRuns = [];
-        scheduledRuns[scheduledRuns.length] = function(){
-          function print(msg){
-            out.getSession().setValue(out.getSession().getValue() + msg);
-          }
-
-          out.getSession().setValue("");
-          runner(print, "runner").run([src.getSession().getValue(), spec.getSession().getValue()]);
-
-          iframe.src = iframe.src;
-        }
+  function delayedRun(){
+    scheduledRuns = [];
+    scheduledRuns[scheduledRuns.length] = function(){
+      function print(msg){
+        out.getSession().setValue(out.getSession().getValue() + msg);
       }
 
-      spec.getSession().addEventListener("change", delayedRun);
-      src.getSession().addEventListener("change", delayedRun);
-    });
-  });
+      out.getSession().setValue("");
+      runner(print, "runner").run([src.getSession().getValue(), spec.getSession().getValue()]);
+
+      iframe.src = iframe.src;
+    }
+  }
+
+  spec.getSession().addEventListener("change", delayedRun);
+  src.getSession().addEventListener("change", delayedRun);
 });
