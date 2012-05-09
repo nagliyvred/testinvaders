b79af70ee2c2019cad378a31a859dbca11af4942
Scott Robinson <sr@thoughtworks.com>
2012-05-09 20:23:13 +0800

    Bullets are hittable. Also, move in Y instead of X.

--- public/javascript/game/bullet.js
+++ public/javascript/game/bullet.js
@@ -1,6 +1,17 @@
-function Bullet(velocity) {
+function Bullet(velocity, x, y) {
+  this.y = y;
+
   this.update = function(dt) {
-    this.x = dt * velocity;
+    this.y = dt * velocity;
+  };
+
+  this.box = function() {
+    return {
+      x: x,
+      y: y,
+      width: 4,
+      height: 12,
+    };
   };
 
   return this;
--- public/javascript/game/spec/bullet_spec.js
+++ public/javascript/game/spec/bullet_spec.js
@@ -1,22 +1,32 @@
 describe("Bullet", function() {
-
   describe("when it is shot", function() {
     it("should move vertically at some velocity", function() {
       var velocity = Math.random();
       var dt = Math.random();
-      var bullet = new Bullet(velocity);
-      bullet.x = 0;
+      var y = Math.random();
+      var bullet = new Bullet(velocity, 0, y);
+
       bullet.update(dt);
-      expect(bullet.x).toEqual(dt * velocity);
+
+      expect(bullet.y).toEqual(y + (dt * velocity));
     });
 
     it("should appear like a white streak");
-    it("should be hittable");
+    it("should be hittable", function() {
+      var x = Math.random(), y = Math.random();
+      var bullet = new Bullet(0, x, y);
+
+      var box = bullet.box();
+
+      expect(box.x).toBe(x);
+      expect(box.y).toBe(y);
+      expect(box.width).toBe(4);
+      expect(box.height).toBe(12);
+    });
 
     describe("when it is hit", function() {
       it("should disappear");
       it("should not be hittable");
     });
   });
-
 });
