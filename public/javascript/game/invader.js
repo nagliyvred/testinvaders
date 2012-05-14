0d3fd653afdedec512e9ed10575b58a56ebbe6fd
Scott Robinson <sr@thoughtworks.com>
2012-05-15 06:09:27 +1000

    AN INVADER APPROACHES RAPIDLY

---
+++ public/javascript/game/invader.js
@@ -0,0 +1,24 @@
+function Invader(initial_velocity, initial_x, initial_y) {
+  var velocity = initial_velocity || 15;
+  var x = initial_x || 0;
+  var y = initial_y || 0;
+
+  this.update = function(dt) {
+    x += dt * velocity;
+  };
+
+  this.draw = function(painter) {
+    painter.draw_invader(x, y);
+  };
+
+  this.box = function() {
+    return {
+      x: x,
+      y: y,
+      width: 66,
+      height: 48
+    };
+  };
+
+  return this;
+}
---
+++ public/javascript/game/spec/invader_spec.js
@@ -0,0 +1,47 @@
+describe("Invader", function() {
+  var invader;
+  var velocity, x, y;
+
+  beforeEach(function() {
+    velocity = Math.random();
+    x = Math.random();
+    y = Math.random();
+
+    invader = new Invader(velocity, x, y);
+  });
+
+  it("should be hittable", function() {
+    var box = invader.box();
+
+    expect(box.x).toEqual(x);
+    expect(box.y).toEqual(y);
+    expect(box.height).toBeGreaterThan(0);
+    expect(box.width).toBeGreaterThan(0);
+  });
+
+  it("should look like blatant copyright infringement", function() {
+    var stub_painter = {draw_invader: jasmine.createSpy("stub_painter.draw_invader")};
+    invader.draw(stub_painter);
+    expect(stub_painter.draw_invader).toHaveBeenCalledWith(x, y);
+  });
+
+  describe("when updated", function() {
+    var stub_input = {};
+
+    beforeEach(function() {
+      invader.update(1, stub_input);
+    });
+
+    it("should move laterally", function() {
+      var new_box = invader.box();
+
+      expect(new_box.x).toEqual(x + velocity);
+      expect(new_box.y).toEqual(y);
+    });
+  });
+
+  describe("when told to invade", function() {
+    xit("should move toward the player's tank");
+    xit("should invert its lateral velocity");
+  });
+});
