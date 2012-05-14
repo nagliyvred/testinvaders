03aaa977e94a88e62320af422cb05915972a4b03
Scott Robinson <sr@thoughtworks.com>
2012-05-15 05:50:46 +1000

    A swarm doesn't do anything yet. And it isn't a rendered object. But it has a pending spec.

--- public/javascript/game/spec/swarm_spec.js
+++ public/javascript/game/spec/swarm_spec.js
@@ -7,21 +7,7 @@ describe("Swarm", function() {
     swarm = new Swarm(invaders);
   });
 
-  it("should draw all its invaders", function() {
-    var stub_painter = {id: "stub_painter"};
-    stub_invader.draw = jasmine.createSpy("stub_invader.draw");
-
-    swarm.draw(stub_painter);
-
-    expect(stub_invader.draw).toHaveBeenCalledWith(stub_painter);
-  });
-
-  it("should update all its invaders", function() {
-    var dt = Math.random();
-    var stub_input = {id: "stub_input"};
-    stub_invader.update = jasmine.createSpy("stub_invader.update");
-
-    swarm.update(dt, stub_input);
-    expect(stub_invader.update).toHaveBeenCalledWith(dt, stub_input);
+  describe("when one of the invaders hits the edge of the screen", function() {
+    xit("should tell all the invaders to INVADE")
   });
 });
