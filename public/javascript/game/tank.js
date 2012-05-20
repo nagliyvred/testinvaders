9eab099d0173d20d68b51d5d627389befe15c644
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-05-20 22:01:27 +1000

    give arbitrary offset to new tank bullet so that it doesn't collide with tank

--- public/javascript/game/tank.js
+++ public/javascript/game/tank.js
@@ -10,7 +10,9 @@ function Tank(bullet) {
     this.box.x = input.mouse.x - (this.box.width / 2);
 
     if (input.pressed("shoot")) {
-      bullet.shoot(-200, input.mouse.x, this.box.y);
+      //TODO: remove 25; this is arbitary offset until we make the tank bullet a seperate 
+      //concept. if we don't do the offset then it collides with the tank immediately
+      bullet.shoot(-200, input.mouse.x, this.box.y - 25);
     }
   };
 
