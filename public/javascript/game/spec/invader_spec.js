17d83575bc9ba82afac49c45e22a1b43421a1874
Mike Gardiner <conversationing@gmail.com>
2012-05-21 22:12:43 +0800

    Refactoring the Invader shooting specs, reworded 'countdown up' to 'timer elapsed'

--- public/javascript/game/spec/invader_spec.js
+++ public/javascript/game/spec/invader_spec.js
@@ -46,11 +46,12 @@ describe("Invader", function() {
       expect(new_box.position.y).toEqual(y);
     });
 
-    describe("shooting", function() {
+    describe("shooting a bullet", function() {
 
       var time_elapsed_until_the_next_shot;
       var minimum_time_between_shots = 20;
-      var countdown_up = function() {
+
+      var elapse_shot_timer = function() {
         invader.update(time_elapsed_until_the_next_shot);
         time_elapsed_until_the_next_shot = minimum_time_between_shots;
       };
@@ -59,35 +60,40 @@ describe("Invader", function() {
         time_elapsed_until_the_next_shot = shot_countdown;
       });
 
-      it("should not shoot a bullet within twenty seconds of firing one before", function() {
-        countdown_up();
-        invader.update(19);
-        expect(stub_bullet.shoot.callCount).toEqual(1);
+      describe("shooting when the timer has elapsed", function() {
+
+        beforeEach(function() {
+          elapse_shot_timer();
+        });
+
+        it("should shoot a bullet when the shot timer elapses", function() {
+          expect(stub_bullet.shoot).toHaveBeenCalled();
+        });
+
+        it("should shoot the bullet from the middle of the invader", function() {
+          expect(stub_bullet.shoot).toHaveBeenCalledWith(jasmine.any(Number), x + (width / 2), y + (height / 2));
+        });
+
+        it("should not shoot a bullet within twenty seconds of firing one before", function() {
+          invader.update(19);
+          expect(stub_bullet.shoot.callCount).toEqual(1);
+        });
+
+        it("should shoot a bullet every time the shot timer elapses", function() {
+          elapse_shot_timer();
+
+          expect(stub_bullet.shoot.callCount).toEqual(2);
+        });
       });
 
-      it("should shoot the bullet from the middle of the invader", function() {
-        countdown_up();
-        expect(stub_bullet.shoot).toHaveBeenCalledWith(jasmine.any(Number), x + (width / 2), y + (height / 2));
-      });
+      describe("shooting when time elapses in increments less than a full second", function() {
+        it("should shoot a bullet when the minimum time elapses" , function() {
+          invader.update(time_elapsed_until_the_next_shot - 1);
+          invader.update(0.999);
+          invader.update(0.010);
 
-      it("should shoot a bullet when the shot countdown is first up", function() {
-        countdown_up();
-        expect(stub_bullet.shoot).toHaveBeenCalled();
-      });
-
-      it("should shoot a bullet every time the shot countdown is up", function() {
-        countdown_up();
-        countdown_up();
-
-        expect(stub_bullet.shoot.callCount).toEqual(2);
-      });
-
-      it("should shoot a bullet when update is signalled in less than full seconds" , function() {
-        invader.update(time_elapsed_until_the_next_shot - 1);
-        invader.update(0.999);
-        invader.update(0.010);
-
-        expect(stub_bullet.shoot).toHaveBeenCalled();
+          expect(stub_bullet.shoot).toHaveBeenCalled();
+        });
       });
 
       describe("staggered bullets", function() {
