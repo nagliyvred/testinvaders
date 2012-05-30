5ab08a02b3c0fb1acdb33c9714ae06819593376e
Scott Robinson <sr@thoughtworks.com>
2012-05-30 16:45:33 +1000

    Only close the drop down when a drop down menu item has been clicked.

--- public/javascript/ext/bootstrap-dropdown.js
+++ public/javascript/ext/bootstrap-dropdown.js
@@ -91,7 +91,7 @@
    * =================================== */
 
   $(function () {
-    //$('html').on('click.dropdown.data-api', clearMenus)
+    $('html').on('click.dropdown.data-api', '.dropdown-menu', clearMenus)
     $('body')
       .on('click.dropdown', '.dropdown form', function (e) { e.stopPropagation() })
       .on('click.dropdown.data-api', toggle, Dropdown.prototype.toggle)
