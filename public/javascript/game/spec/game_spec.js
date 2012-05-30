e627158c28d5664d51456a972f95b423b10ee482
Scott Robinson <sr@thoughtworks.com>
2012-05-30 16:47:57 +1000

    Inline the atom spec helper.

--- public/javascript/game/spec/game_spec.js
+++ public/javascript/game/spec/game_spec.js
@@ -1,3 +1,18 @@
+// Test helper for the Atom microframework
+var atom = {};
+
+atom.Game = function() {
+  return {};
+};
+
+atom.input = {
+  mouse: {
+    x: Math.random(),
+    y: Math.random()
+  }
+};
+
+// Our spec!
 describe("Given a Game", function() {
   var game, painter, thing, collision;
 
